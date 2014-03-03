3a1cadc9b405068ab1bf118ab3b6dc2875a7a126
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2014-03-03 21:48:30 +0100

    Use ArrayList of colors instead of counter in histogram

--- src/ColorHistogram.java
+++ src/ColorHistogram.java
@@ -4,12 +4,17 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class ColorHistogram {
-    private int[][][] mHistogram;
+    private ArrayList<Color>[][][] mHistogram;
     private int mDivisionsNumber;
 
     public ColorHistogram(int divisions, Color... colors) {
         mDivisionsNumber = divisions;
-        mHistogram = new int[divisions][divisions][divisions];
+        mHistogram = new ArrayList[divisions][divisions][divisions];
+        for (int i = 0; i < divisions; i++)
+            for (int j = 0; j < divisions; j++)
+                for (int k = 0; k < divisions; k++) {
+                    mHistogram[i][j][k] = new ArrayList<Color>();
+                }
         for (Color color : colors) {
             addPixel(color);
         }
@@ -17,7 +22,7 @@ public class ColorHistogram {
 
     private void addPixel(Color color) {
         int divisionLength = 256 / mDivisionsNumber;
-        mHistogram[color.getRed() / divisionLength][color.getGreen() / divisionLength][color.getBlue() / divisionLength]++;
+        mHistogram[color.getRed() / divisionLength][color.getGreen() / divisionLength][color.getBlue() / divisionLength].add(color);
     }
 
     ColorBox getMainDivision() {
@@ -27,13 +32,13 @@ public class ColorHistogram {
             for (int j = 0; j < mDivisionsNumber; j++)
                 for (int k = 0; k < mDivisionsNumber; k++) {
                     System.out.println("[" + i + "][" + j + "][" + k + "] = " + mHistogram[i][j][k]);
-                    if (mHistogram[i][j][k] >= mHistogram[result.red][result.green][result.blue]) {
+                    if (mHistogram[i][j][k].size() >= mHistogram[result.red][result.green][result.blue].size()) {
                         System.out.println(mHistogram[i][j][k] +" >= " + mHistogram[result.red][result.green][result.blue]);
                         System.out.println("found new max " + mHistogram[i][j][k]);
                         result.red = i;
                         result.green = j;
                         result.blue = k;
-                        result.count = mHistogram[i][j][k];
+                        result.count = mHistogram[i][j][k].size();
                     }
                 }
         System.out.println(" max [" + result.red + "][" + result.green + "][" + result.blue + "]");
@@ -59,7 +64,7 @@ public class ColorHistogram {
         for (int i = 0; i < mDivisionsNumber; i++)
             for (int j = 0; j < mDivisionsNumber; j++)
                 for (int k = 0; k < mDivisionsNumber; k++) {
-                    result.add(new ColorBox(i,j,k, mHistogram[i][j][k]));
+                    result.add(new ColorBox(i,j,k, mHistogram[i][j][k].size()));
                 }
 
         Collections.sort(result, new ColorBoxComparator());
